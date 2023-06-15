9787686de30ab72a09887109467f28b47a3bf00e
ykawabata17 <relu_1313@icloud.com>
2023-06-15 13:11:36 +0900

    Add Validation of Presence, Length, Format

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -22,4 +22,43 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "   "
     assert_not @user.valid?
   end
+
+  # 長さの検証(user name)
+  test "name should not be too long" do
+    @user.name = "a" * 51
+    assert_not @user.valid?
+  end
+
+  # 長さの検証(email)
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  # フォーマットの検証(有効性)
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  # フォーマットの検証(無効性)
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert @user.invalid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
+  # 一意性の検証
+  test "email addresses should be unique" do
+    # .dupで同じ属性を持つデータを複製する
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end
