b4a532250963af94133cadeb66515014b81957ad
ykawabata17 <relu_1313@icloud.com>
2023-06-16 15:58:35 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      reset_session
+      log_in @user
       flash[:success] = "Welcom to the sample App!"
       redirect_to @user
     else
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,22 +1,41 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  
-  test "login with invalid information" do
-    # ログイン用のパスを開く
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid email/invalid password" do
     get login_path
-    # 新しいセッションのフォームが正しく表示されたことを確認
     assert_template 'sessions/new'
-    # わざと無効なparamsハッシュを使ってセッション用パスにPOSTする
-    post login_path, params: { session: { email: "", password: "" } }
-    # 新しいセッションのフォームが正しいステータスを返し，再度表示されることを確認する
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
+    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_template 'sessions/new'
-    # フラッシュメッセージが表示されることを確認する
     assert_not flash.empty?
-    # 別のページに一旦移動
     get root_path
-    # 移動さきのページでフラッシュメッセージが表示されていないことを確認する
     assert flash.empty?
+  end
+
+  test "login with valid information followed by logout" do
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 end
